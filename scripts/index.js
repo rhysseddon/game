5ca3acfd670596fb4723a62293798610460c8fe8
rhysseddon <rhysseddon@hotmail.com>
2020-11-12 09:58:19 +0000

    added jquery functions to answers and added coresponding css

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+let score = 0;
+
+
+$('.answer1').click(function(){
+$('.color').css('background-color', 'purple');
+$('h2').text('correct answer!');
+$
+});
+$('.answer2').click(function(){
+$('.color').css('background-color', 'white');
+$('h2').text('wrong answer!');
+});
+$('.answer3').click(function(){
+$('.color').css('background-color', 'white');
+$('h2').text('wrong answer!');
+$('h1').text('Score:1');
+});
+$('.answer4').click(function(){
+$('.color').css('background-color', 'white');
+$('h2').text('wrong answer!');
+});
+
